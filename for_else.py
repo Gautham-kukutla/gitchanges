f12d6c934e9135ba2d29689019fcc7e4a811ba89
Gautham-kukutla <gautham.kukutla@gmail.com>
2022-03-15 02:55:20 -0700

    Commit done

--- for_else.py
+++ for_else.py
@@ -1,12 +1,11 @@
 # for i in range (1,8,2):
 #     print(i)
 
-for i in range(10) :
-    
-    
+for i in range(10):
+
     print(i)
 
-else :
-   
+else:
+
 
 print("This is inside else of for")
--- python_script.py
+++ python_script.py
@@ -7,7 +7,7 @@ print('Remotes:')
 for remote in repo.remotes:
     print(f'- {remote.name} {remote.url}')
 print("remote after")
-#comment
+# comment
 '''with repo.config_writer() as git_config:
     git_config.set_value('user', 'email', 'gautham.kukutla@gmail.com')
     git_config.set_value('user', 'name', 'Gautham-kukutla')
@@ -15,15 +15,15 @@ with repo.config_reader() as git_config:
     print(git_config.get_value('user', 'email'))
     print(git_config.get_value('user', 'name'))'''
 if repo.is_dirty(untracked_files=True):
-  print("Changes Detected")
-  repo.git.add(all=True)
-  repo.index.commit("Commit Done")
-  print("pushing")
-  f = open("changes.txt", "x")
-  #for branch in repo.branches:
-  #  print(branch)
-  # repo.git.checkout('main')
-  #print(repo.remotes.origin.push())
-  #repo.git.push('--set-upstream', 'origin', 'main')
+    print("Changes Detected")
+    repo.git.add(all=True)
+    repo.index.commit("Commit Done")
+    print("pushing")
+    f = open("changes.txt", "x")
+    # for branch in repo.branches:
+    #  print(branch)
+    # repo.git.checkout('main')
+    # print(repo.remotes.origin.push())
+    #repo.git.push('--set-upstream', 'origin', 'main')
 else:
-  print("No Changes")
+    print("No Changes")
