e5ea3de0005b7df16b35bdf7442b5d38ecca6692
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-15 14:42:55 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,17 @@
-#!bin/bash
-for i in *.py
- do
-  autopep8 --in-place --aggressive --aggressive $i
-  echo $i
- done
-
+#!/bin/bash
+#apt-get install python-autopep8
+find . -name '*.py' > file.txt
+#cat file.txt
+ while read -r line
+ do autopep8 --in-place --aggressive --aggressive "$line"
+ done < file.txt
+ 
+ git status | grep ".py$" > file.txt
+cat file.txt
+ if [ -s file.txt ]
+ then   rm file.txt
+        git add .
+        git commit -am "Commit done"
+        git push "https://github.com/Gautham-kukutla/pythontest.git"
+ else  echo "no changes done"
+ fi
