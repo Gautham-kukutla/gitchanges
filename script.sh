8632a8158ac0f2537743f926a776c471b0478add
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-15 15:14:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,6 +12,6 @@ cat file.txt
  then   rm file.txt
         git add .
         git commit -am "Commit done"
-        git push "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Gautham-kukutla/pythongitchanges.git"
+        git push "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Gautham-kukutla/gitchanges.git"
  else  echo "no changes done"
  fi
