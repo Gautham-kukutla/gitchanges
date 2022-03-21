747a8ec9195b3efec3584da0f89eeedd40d8cd2d
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-21 11:56:35 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 #apt-get install python-autopep8
-find . -name '*.py' > file.txt
-#cat file.txt
- while read -r line
- do autopep8 --in-place --aggressive --aggressive "$line"
- done < file.txt
- 
- git status | grep ".py$" > file.txt
-cat file.txt
- if [ -s file.txt ]
- then   rm file.txt
-        git add .
-        git commit -am "Commit done"
+ autopep8 --exit-code --recursive --in-place --aggressive --aggressive .
+
+ if [ steps.autopep8.outputs.exit-code == 2 ]
+ then   git commit -am "Automated autopep8 fixes"
         git config user.email "gautham.kukutla@gmail.com"
         git config user.name "Gautham-kukutla"
-        git push "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Gautham-kukutla/gitchanges.git"
+        git push "https://github.com/Gautham-kukutla/gitchanges.git"
  else  echo "no changes done"
  fi
