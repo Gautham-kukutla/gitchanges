a9d6b3530f0a1da7b820aba8f984a170605b4ac8
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-22 12:38:46 +0530

    Update for_else1.py

--- for_else1.py
+++ for_else1.py
@@ -1,12 +1,18 @@
 student_name = input("Please enter student Name:")
 
-marks = {'arjun': 90, 'kiran': 55, 'raju': 77}
+marks = {'arjun':  90, 'kiran':  55, 'raju': 77}
 
-for student in marks:
+for student in marks :
 
-    if student == student_name:
+    if student == student_name :
+        
+        
+        
         print(marks[student])
         break
+        
+        
+        
 else:
 
     print('No entry with that name found.')
