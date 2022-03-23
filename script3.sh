4bdad30baefb39ef72cc906f8c822acfc8533f6f
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-23 12:03:29 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+var=0
 find . -name '*.py' > file2
 while read -r line 
 do autopep8 --in-place --exit-code --aggressive --aggressive "$line"
    if [ $? -eq 2 ] 
-	then	git add "$line"
+	then	var=1
+		git add "$line"
    fi
 done < file2
-git status | grep ".py$" > file2
-if  [ -s file2 ]
+#git status | grep ".py$" > file2
+if  [ "$var" -eq 1 ]
 then  rm file2
 	git commit -m "autopep8 changes done"
 	git push "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${GIT_USERNAME}/gitchanges.git" HEAD:main
