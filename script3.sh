4a5de8443c5570ab8bf8c3bdb0c9984b65419706
Gautham-kukutla <36068420+Gautham-kukutla@users.noreply.github.com>
2022-03-23 12:41:17 +0530

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -10,12 +10,12 @@ do autopep8 --in-place --exit-code --aggressive --aggressive "$line"
 		git add "$line"
    fi
 done < file2
-git status
+#git status
 #git status | grep ".py$" > file2
 if  [ "$var" -eq 1 ]
 then  rm file2
 	git commit -m "autopep8 changes done"
-#	git push "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${GIT_USERNAME}/gitchanges.git"
+	git push "https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/${GIT_USERNAME}/gitchanges.git"
 else	rm file2
 	echo "no autopep8 changes"
 fi
